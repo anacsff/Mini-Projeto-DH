dc145bab7e36ab7a179ae39537bbdc76291b31bf
anacsff <anacsfonseca@outlook.com.br>
2022-10-10 21:48:39 -0300

    Arquivo .sql e modelo do banco de dados

---
+++ src/data/apollotech_db.sql
@@ -0,0 +1,42 @@
+DROP DATABASE IF EXISTS apollotech_db;
+
+-- Cria banco de dados
+CREATE DATABASE IF NOT EXISTS apollotech_db;
+
+-- Seleciona banco de  dados 
+USE apollotech_db;
+
+-- Cria tabela de cursos
+CREATE TABLE cursos(
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50)
+);
+
+-- Inserindo cursos na tabela cursos
+INSERT INTO cursos  (nome)
+VALUES
+	("Desenvolvimento Web"),
+    ("Desenvolvimento Android Mobile"),
+    ("Desenvolvimento Back-end"),
+    ("Segurança da Informação"),
+    ("UX/UI Designer");
+    
+-- Cria tabela de alunos
+CREATE TABLE alunos (
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+	data_nascimento DATE NOT NULL,
+    celular VARCHAR(11) NOT NULL,
+    curso_id INT UNSIGNED,
+    FOREIGN KEY (curso_id) REFERENCES cursos(id)
+);
+
+INSERT INTO alunos (nome, email, data_nascimento, celular, curso_id)
+VALUES
+	("Ana Carolina de Souza Fonseca", "anacsfonseca@hotmail.com.br", "1998-10-08", "11934698231", 1),
+    ("Inês de Souza", "mi.souza2011@gmail.com", "1968-07-08", "11933543675", 2),
+    ("Nicollas Geronassi da Silva", "nicollasgs@gmail.com", "2004-02-04", "11913523160", 5),
+    ("Duda Souza", "mariadudinha2010@hotmail.com", "2010-08-17", "11934689213", 4);
+    
+SELECT * FROM alunos
